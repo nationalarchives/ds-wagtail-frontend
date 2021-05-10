06e5ee610c0623cc1fc050ebd2bd9e5f4a51a207
Gwyn Jones <m@gwnjns.com>
2021-05-10 14:58:02 +0100

    Remove phase banner
    Cross browser testing has revealed this approach is problematic. Will
    suggest another approach is taken.

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,6 @@
                     <img src="/images/tna-blk-logo.svg" class="img-fluid tna-header__logo"
                          alt="The National Archives">
                 </a>
-                <span class="tna-header__phase-notice">BETA</span>
             </div>
         </div>
     </div>
