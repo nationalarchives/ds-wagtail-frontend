2b387969d3298178d963f69e806138ad73c44262
AshTNA <ashleyaburnett@gmail.com>
2021-05-10 14:12:55 +0100

    Add breadcrumbs and explorer heading section

--- collection-explorer/explore_the_collection.php
+++ collection-explorer/explore_the_collection.php
@@ -10,10 +10,24 @@
 <?php require '../includes/header.php' ?>
 
 <main>
+    <div class="explorer-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumbs">
+                        <a class="breadcrumbs__link" href="/">Home</a>
+                        <span class="breadcrumbs__seperator"> > </span>
+                        <span class="breadcrumbs__current"><?php echo $page_title ?></span>
+                    </div>
+                    <h1 class="explorer-header__title"><?php echo $page_title ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <div class="row">
             <div class="col-md-10" id="content">
-                <h1><?php echo $page_title ?></h1>
             </div>
         </div>
     </div>
