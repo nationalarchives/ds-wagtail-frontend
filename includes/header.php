f70fe186bd0b5a36287929ce87b3b8453f0273f2
Gwyn Jones <m@gwnjns.com>
2021-05-10 14:21:27 +0100

    Update header to reflect design
    This includes adding --phase-notice element.

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,12 @@
-<header class="tna-header tna-bg--dark">
+<header class="tna-header">
     <div class="container">
         <div class="row">
             <div class="col-3">
-                <a href="#">
-                    <img src="/images/tna-wh-logo.svg" class="img-fluid tna-header__logo"
+                <a href="/">
+                    <img src="/images/tna-blk-logo.svg" class="img-fluid tna-header__logo"
                          alt="The National Archives">
                 </a>
+                <span class="tna-header__phase-notice">BETA</span>
             </div>
         </div>
     </div>
