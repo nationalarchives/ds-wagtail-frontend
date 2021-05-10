22fc4cd3fc8d87bd11e7686c619223ec323aad64
Gwyn Jones <m@gwnjns.com>
2021-05-10 13:09:57 +0100

    Add Explore the Collection page (and an index page)

---
+++ collection-explorer/explore_the_collection.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<?php $page_title = "Explore the collection" ?>
+
+<?php require '../includes/head.php' ?>
+
+<body class="template-homepage">
+
+<?php require '../includes/header.php' ?>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10" id="content">
+                <h1><?php echo $page_title ?></h1>
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<?php require '../includes/footer.php' ?>
+
+<script type="text/javascript" src="/static/js/etna.js"></script>
+
+</body>
+</html>
+
+
