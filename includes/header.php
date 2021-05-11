d7f48ba65d6d11e86ea57111087272f8e2cbd991
Gwyn Jones <m@gwnjns.com>
2021-05-11 08:09:09 +0100

    Introduce BETA version of logo (as SVG)

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-3">
                 <a href="/">
-                    <img src="/images/tna-blk-logo.svg" class="img-fluid tna-header__logo"
+                    <img src="/images/tna-blk-logo-beta.svg" class="img-fluid tna-header__logo--beta"
                          alt="The National Archives">
                 </a>
             </div>
