a3ac57fa1e6ea901405afb1684e439ad7dfa5e08
AshTNA <ashleyaburnett@gmail.com>
2021-05-12 11:26:51 +0100

    Add alt tags to card images

--- includes/card-group-secondary-nav.php
+++ includes/card-group-secondary-nav.php
@@ -2,7 +2,7 @@
     <article class="card-group-secondary-nav">
         <a href="$card_url" class="card-group-secondary-nav__link">
             <div class="card-group-secondary-nav__image">
-                <img src= <?php echo "/images/collection-explorer/$card_image"?>/>
+                <img <?php echo "src='/images/collection-explorer/$card_image' alt='$card_title'"?> />
             </div>
             <div class="card-group-secondary-nav__body">
                 <h3><?php echo $card_title ?></h3>
