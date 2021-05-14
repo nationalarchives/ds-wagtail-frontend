a999a0a2ef0ac77ae19f40d0d1d2dc9b247ecfd4
AshTNA <ashleyaburnett@gmail.com>
2021-05-14 11:10:09 +0100

    Add social media icons to footer and format footer

--- includes/footer.php
+++ includes/footer.php
@@ -11,10 +11,9 @@
         </div>
     </div>
 
-    <div class="tna-footer__base tna-bg--dark pb-3">
+    <div class="tna-footer__body tna-bg--dark pb-3">
         <div class="container">
-
-        <div class="row">
+            <div class="row">
                 <div class="col-6 col-md-3">
                     <h4>Find out more</h4>
                     <ul class="tna-ul--no-bullet">
@@ -51,23 +50,51 @@
                     </ul>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="tna-footer__social-media-links tna-bg--dark pb-3">
+        <div class="container">
+            <div class="row justify-content-md-center text-center">
+                <div class="col-4 col-sm-4 col-md-1">
+                    <i class="fab fa-twitter"></i>
+                </div>
+                <div class="col-4 col-sm-4 col-md-1">
+                    <i class="fab fa-youtube"></i>
+                </div>
+                <div class="col-4 col-sm-4 col-md-1">
+                    <i class="fab fa-flickr"></i>
+                </div>
+                <div class="col-4 col-sm-4 col-md-1">
+                    <i class="fab fa-facebook-square"></i>
+                </div>
+                <div class="col-4 col-sm-4 col-md-1">
+                    <i class="fab fa-instagram"></i>
+                </div>
+                <div class="col-4 col-sm-4 col-md-1">
+                    <i class="fas fa-rss"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="tna-footer__base tna-bg--dark pb-3">
+        <div class="container">
 
             <div class="row justify-content-md-center">
-                <div class="col-md-1 text-center">
-                    <img src="/images/logo-ogl.png" alt="Open Government License logo" id="ogl-logo">
+                <div class="col-md-2 text-center">
+                    <img src="/images/logo-ogl.png" alt="Open Government License logo" id="ogl-logo" class="img-fluid">
                 </div>
-                <div class="col-md-8 text-center">
-                    <p>
+                <div class="col-md-8 text-center align-self-center">
+                    <p class="mb-0">
                         All content is available under the
                         <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/"
                            title="External website - opens in a new window" target="_blank" rel="noopener noreferrer">
                             Open Government Licence v3.0</a>, except where otherwise stated
                     </p>
                 </div>
-                <div class="col-md-1 text-center">
+                <div class="col-md-2 text-center">
                     <a title="External website - opens in a new window" href="http://gov.uk/" target="_blank"
                        rel="noopener noreferrer">
-                        <img alt="GOV.UK logo" src="/images/logo-gov-uk.png">
+                        <img alt="GOV.UK logo" src="/images/logo-gov-uk.png" class="img-fluid">
                         <span class="sr-only">Opens a new window</span>
                     </a>
                 </div>
--- includes/head.php
+++ includes/head.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet">
 
-
+    <link rel="stylesheet" href="/css/libraries/font-awesome/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="/css/libraries/tna-toolkit.0.0.1.css">
     <link rel="stylesheet" type="text/css" href="/css/dist/etna.css">
 
