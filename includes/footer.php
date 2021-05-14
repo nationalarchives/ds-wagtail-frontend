5ab2c83c13a8632522123c54a5f761289f7697c1
AshTNA <ashleyaburnett@gmail.com>
2021-05-14 10:45:51 +0100

    Populate footer with links

--- includes/footer.php
+++ includes/footer.php
@@ -13,6 +13,45 @@
 
     <div class="tna-footer__base tna-bg--dark pb-3">
         <div class="container">
+
+        <div class="row">
+                <div class="col-6 col-md-3">
+                    <h4>Find out more</h4>
+                    <ul class="tna-ul--no-bullet">
+                        <li><a href="">Contact us</a></li>
+                        <li><a href="">Press room</a></li>
+                        <li><a href="">Jobs and careers</a></li>
+                        <li><a href="">Friends of The National Archives</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-3">
+                    <h4>Site help</h4>
+                    <ul class="tna-ul--no-bullet">
+                        <li><a href="">Help</a></li>
+                        <li><a href="">Website A-Z index</a></li>
+                        <li><a href="">Accessibility</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-3">
+                    <h4>Websites</h4>
+                    <ul class="tna-ul--no-bullet">
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Podcasts and videos</a></li>
+                        <li><a href="">Shop</a></li>
+                        <li><a href="">Image library</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-3">
+                    <h4>Legal</h4>
+                    <ul class="tna-ul--no-bullet">
+                        <li><a href="">Terms of use</a></li>
+                        <li><a href="">Privacy policy</a></li>
+                        <li><a href="">Cookies</a></li>
+                        <li><a href="">Our fees</a></li>
+                    </ul>
+                </div>
+            </div>
+
             <div class="row justify-content-md-center">
                 <div class="col-md-1 text-center">
                     <img src="/images/logo-ogl.png" alt="Open Government License logo" id="ogl-logo">
