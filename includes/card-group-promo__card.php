7127d49bd5486f29c73ae16d311d624d87a66989
AshTNA <ashleyaburnett@gmail.com>
2021-05-19 11:18:06 +0100

    Improve promo reflow

--- includes/card-group-promo__card.php
+++ includes/card-group-promo__card.php
@@ -1,12 +1,12 @@
 
 <div class="card-group-promo__card">
     <div class='row'>
-        <div class='col-lg-4 pl-0'>
+        <div class='col-md-12 col-lg-5 col-xl-4'>
             <a href="<?php echo $card_values['link'] ?>">
                 <img src="<?php echo $card_values["image"]?>" alt="<?php echo $card_values["image_alt"]?>" class='card-group-promo__card-image'/>
             </a>
         </div>
-        <div class='col-lg-8'>
+        <div class='col-md-12 col-lg-7 col-xl-8'>
             <h3 class="card-group-promo__card-heading"><a href="<?php echo $card_values['link'] ?>"><?php echo $card_title?></a></h3>
             <p><?php echo $card_values["description"]?></p>
         </div>
