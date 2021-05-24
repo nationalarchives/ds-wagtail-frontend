967dc9bc72486d994d8b47e3995f782f966f0199
Gwyn Jones <m@gwnjns.com>
2021-05-24 15:19:45 +0100

    Add accessibility enhancements to pagination

--- includes/pagination.php
+++ includes/pagination.php
@@ -1,12 +1,20 @@
 <!-- Whitespace between <li> elements affects the look of this component on mobile. -->
-<div class="pagination">
-  <ol class="pagination__list">
-    <li class="pagination__list-item">
-      <a class="pagination__page-chevron-previous" href="#"><span class="sr-only">Previous page</span></a>
-    </li><li class="pagination__list-item">
-        <a class="pagination__page-link-current" href="#">1</a>
-    </li><?php for ($i=2; $i <= 10 ; $i++): ?><li class="pagination__list-item"><a class="pagination__page-link" href="#"><?php echo $i ?></a></li><?php endfor; ?><li class="pagination__list-item">
-      <a class="pagination__page-chevron-next" href="#"><span class="sr-only">Next page</span></a>
-    </li>
-  </ol>
-</div>
+<nav class="pagination" role="navigation" aria-label="Results pagination">
+    <ol class="pagination__list">
+        <li class="pagination__list-item">
+            <a class="pagination__page-chevron-previous" href="#"><span class="sr-only">Previous page</span></a>
+        </li>
+        <li class="pagination__list-item">
+            <a class="pagination__page-link-current" href="#" aria-label="Current page, Page 1"
+               aria-current="true">1</a>
+        </li>
+        <?php for ($i = 2; $i <= 10; $i++): ?>
+            <li class="pagination__list-item">
+                <a class="pagination__page-link" href="#" aria-label="Go to page <?= $i ?>"><span class="sr-only">Page </span><?= $i ?></a>
+            </li>
+        <?php endfor; ?>
+        <li class="pagination__list-item">
+            <a class="pagination__page-chevron-next" href="#"><span class="sr-only">Next page</span></a>
+        </li>
+    </ol>
+</nav>
