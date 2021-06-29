7a684b54645217aa3d7f90457939e346207cdfe2
JamesWChan <jwchan1993@gmail.com>
2021-06-29 11:18:56 +0100

    Add shakespeare insights page data

---
+++ data/insights_shakespeare_data.php
@@ -0,0 +1,14 @@
+<?php
+
+$page = [
+    "title" => "Collection Insights - Shakespeare",
+];
+
+$generic_intro = [
+    'heading' => $page['title'],
+    'text' => "An insight into our records relating to William Shakespeare"
+];
+
+$breadcrumbs = [
+    "Home" => "/"
+];
