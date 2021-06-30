f8469782b15360170dc3aaa93ae292dc38a3f7ef
JamesWChan <jwchan1993@gmail.com>
2021-06-30 10:17:38 +0100

    Include roboto mono font

--- includes/head.php
+++ includes/head.php
@@ -6,8 +6,10 @@
 
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css" href="/css/libraries/tna-toolkit.0.0.1.css">
     <link rel="stylesheet" type="text/css" href="/css/dist/etna.css">
