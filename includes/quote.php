d044f3b94c2bd68cec4d7d477f95b8ae067db892
JamesWChan <jwchan1993@gmail.com>
2021-06-30 10:28:12 +0100

    Refactor quote component implementation

--- includes/quote.php
+++ includes/quote.php
@@ -1,15 +1,13 @@
-<div class="col-md-12">
-    <div class="quote">
-        <img class="quote-icon" src="<?= $quote_values['icon_src']?>" alt="Quote icon" />
-        <blockquote>
-            <p>
-                <?php echo $quote_values['text'] ?>
-            </p>
-        </blockquote>
-        <div class="citation">
-            <cite>
-                <?php echo $quote_values['citation'] ?>
-            </cite>
-        </div>
+<div class="quote">
+    <img class="quote__icon" src="<?= $quote_values['icon_src']?>" alt="Quote icon" />
+    <blockquote class="quote__container">
+        <p class="quote__text">
+            <?php echo $quote_values['text'] ?>
+        </p>
+    </blockquote>
+    <div class="quote__citation">
+        <cite>
+            <?php echo $quote_values['citation'] ?>
+        </cite>
     </div>
 </div>
