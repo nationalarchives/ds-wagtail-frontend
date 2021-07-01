0a0ecb66ab7d35345528e35441b7d5ecdc46b7e2
JamesWChan <jwchan1993@gmail.com>
2021-07-01 20:41:58 +0100

    Adjust callout padding and place within container

--- includes/callout.php
+++ includes/callout.php
@@ -1,21 +1,23 @@
 <div class="callout">
-    <p class="callout__heading">
-        <?php echo $callout_values['text'] ?>
-    </p>
+    <div class="container">
+        <p class="callout__heading">
+            <?php echo $callout_values['text'] ?>
+        </p>
 
-    <ul class="callout__list">
-        <?php
-        foreach ($callout_values['links'] as $callout_links => $callout_link) { ?>
-            <li class="callout__list-item">
-                <a class="callout__link" href="/">
-                    <span class="callout__link-heading">
-                        <?php echo $callout_link['heading'] ?>
-                    </span>
-                    <p class="callout__link-text">
-                        <?php echo $callout_link['text'] ?>
-                    </p>
-                </a>
-            </li>
-        <?php } ?>
-    </ul>
+        <ul class="callout__list">
+            <?php
+            foreach ($callout_values['links'] as $callout_links => $callout_link) { ?>
+                <li class="callout__list-item">
+                    <a class="callout__link" href="/">
+                        <span class="callout__link-heading">
+                            <?php echo $callout_link['heading'] ?>
+                        </span>
+                        <p class="callout__link-text">
+                            <?php echo $callout_link['text'] ?>
+                        </p>
+                    </a>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
 </div>
