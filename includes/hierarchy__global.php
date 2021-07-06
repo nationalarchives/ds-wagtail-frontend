0e51d69f9c12464863a9b207a0bd55c8d06097b8
Matt Blair <mdrblair@gmail.com>
2021-07-06 18:32:52 +0100

    Adding Javascript to handle opening and closing of hierarchy at different screen sizes

--- includes/hierarchy__global.php
+++ includes/hierarchy__global.php
@@ -1,6 +1,6 @@
 <nav class="hierarchy-global hierarchy-global--sticky">
 
-    <details>
+    <details id="js-hierarchy-global" open>
         <summary><div class="hierarchy-global__heading"><h2>Where am I in the catalogue?</h2></div></summary>
 
         <ul class="hierarchy-global__list">
@@ -14,9 +14,54 @@
                 <span class="reference">C 5</span><a href="/journey/record/C/5">Court of Chancery: Six Clerks Office: Pleadings before 1714, Bridges</a></span>
             </li>
             <li class="current">
-                <span class="location"> You are here:</span>
+                <span class="location">You are here:</span>
                 <span class="reference">C 5/139/35</span> Short title: Frisby v Frisby. Plaintiffs: John Frisby, William Frisby, and</span>
             </li>
         </ul>
     </details>
 </nav>
+
+<script>
+
+    // Set the details element as open at 1200px or above
+    var manage_details_element = function() {
+
+        var details_element = document.getElementById('js-hierarchy-global'),
+            summary_element = document.querySelector('#js-hierarchy-global summary');
+
+        if(!!details_element) {
+
+            if(window.innerWidth < 1200) {
+                details_element.removeAttribute('open');
+                summary_element.setAttribute('tabindex', '0');
+            } else {
+                details_element.setAttribute('open', '');
+                summary_element.setAttribute('tabindex', '-1');
+            }
+        }
+    };
+
+    // Prevent the details element from closing at 1200px or above
+    var details_element = document.getElementById('js-hierarchy-global');
+
+    details_element.addEventListener('click', function(e) {
+
+        if(window.innerWidth < 1200) {
+            return;
+        }
+
+        e.preventDefault();
+
+    });
+
+    // Register event handlers
+    document.addEventListener("DOMContentLoaded", function() {
+        manage_details_element();
+    });
+
+    window.addEventListener("resize", function() {
+        manage_details_element();
+    });
+
+
+</script>
