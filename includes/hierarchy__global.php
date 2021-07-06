7b186f9555d60e46db303823ac341f1578bb7c38
Matt Blair <mdrblair@gmail.com>
2021-07-06 16:19:15 +0100

    - Adding details/summary HTML to handle hierarchy on mobile - Small re-styling of heading area to accomodate details/summary

--- includes/hierarchy__global.php
+++ includes/hierarchy__global.php
@@ -1,25 +1,22 @@
 <nav class="hierarchy-global hierarchy-global--sticky">
-    <div class="hierarchy-global__heading">
-        <h2>Where am I in the catalogue?</h2>
-    </div>
-    <ul class="hierarchy-global__list">
-        <li>
-            <span class="reference">C</span><a href="/journey/record/C">Records created, acquired, and inherited by Chancery, and also of the Wardrobe, Royal Household, Exchequer and various commissions</a></span>
-        </li>
 
-        <li>
-            <span class="reference">Division within C</span><a href="/journey/record/C/~15">Records of Equity Side: the Six Clerks</a></span>
-        </li>
+    <details>
+        <summary><div class="hierarchy-global__heading"><h2>Where am I in the catalogue?</h2></div></summary>
 
-
-
-        <li>
-            <span class="reference">C 5</span><a href="/journey/record/C/5">Court of Chancery: Six Clerks Office: Pleadings before 1714, Bridges</a></span>
-        </li>
-        <li class="current">
-            <span class="location"> You are here:</span>
-            <span class="reference">C 5/139/35</span> Short title: Frisby v Frisby. Plaintiffs: John Frisby, William Frisby, and</span>
-
-        </li>
-    </ul>
+        <ul class="hierarchy-global__list">
+            <li>
+                <span class="reference">C</span><a href="/journey/record/C">Records created, acquired, and inherited by Chancery, and also of the Wardrobe, Royal Household, Exchequer and various commissions</a></span>
+            </li>
+            <li>
+                <span class="reference">Division within C</span><a href="/journey/record/C/~15">Records of Equity Side: the Six Clerks</a></span>
+            </li>
+            <li>
+                <span class="reference">C 5</span><a href="/journey/record/C/5">Court of Chancery: Six Clerks Office: Pleadings before 1714, Bridges</a></span>
+            </li>
+            <li class="current">
+                <span class="location"> You are here:</span>
+                <span class="reference">C 5/139/35</span> Short title: Frisby v Frisby. Plaintiffs: John Frisby, William Frisby, and</span>
+            </li>
+        </ul>
+    </details>
 </nav>
