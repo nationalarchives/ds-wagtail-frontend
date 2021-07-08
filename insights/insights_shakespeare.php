16b6f3fe81ea49ec8b3259b6758678f776897fad
JamesWChan <jwchan1993@gmail.com>
2021-07-08 11:52:37 +0100

    Move separator component up in the page

--- insights/insights_shakespeare.php
+++ insights/insights_shakespeare.php
@@ -26,8 +26,8 @@
                 <?php render_media_embeds($media_embed_list); ?>
                 <?php render_research_resources($research_resources_list) ?>
                 <?php render_record_embeds($record_embed_list); ?>
-                <?php render_blog_embeds($blog_embed_list) ?>
                 <?php render_section_separator("Section 3: Lorem ipsum dolor sit amet", "section-3"); ?>
+                <?php render_blog_embeds($blog_embed_list) ?>
                 <h3 class="related-content__heading"><?php echo $page['related_content_heading'] ?></h3>
                 <p><?php echo $page['related_content_description'] ?></p>
                 <div class="row">
