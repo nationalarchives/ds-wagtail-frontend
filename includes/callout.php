3eb1cd71477ec3103db39f85ee6354c8dac1ba98
JamesWChan <jwchan1993@gmail.com>
2021-07-09 11:41:48 +0100

    Refactor callout component
    Add sr-only h3; place links within h4 tags; implement conditional render of links; amend styling accordingly

--- includes/callout.php
+++ includes/callout.php
@@ -1,25 +1,29 @@
 <div class="callout">
     <div class="container">
+        <h3 class="sr-only"><?php echo $callout_values['heading'] ?></h3>
         <p class="callout__standfirst">
             <?php echo $callout_values['standfirst-text'] ?>
         </p>
 
-        <dl class="callout__list">
-            <?php
-            foreach ($callout_values['links'] as $callout_links => $callout_link) { ?>
-                <div class="callout__list-item">
-                    <dt>
-                        <a class="callout__link" href="<?php echo $callout_link['link'] ?>">
-                            <span class="callout__link-heading">
-                                <?php echo $callout_link['heading'] ?>
-                            </span>
-                        </a>
-                    </dt>
-                    <dd class="callout__link-text">
-                        <?php echo $callout_link['text'] ?>
-                    </dd>
-                </div>
-            <?php } ?>
-        </dl>
+        <?php
+            if(!empty($callout_values['links'])){ ?>
+                <dl class="callout__list">
+                    <?php
+                    foreach ($callout_values['links'] as $callout_links => $callout_link) { ?>
+                        <div class="callout__list-item">
+                            <dt>
+                                <h4 class="callout__link-heading">
+                                    <a class="callout__link" href="<?php echo $callout_link['link'] ?>">
+                                        <?php echo $callout_link['heading'] ?>
+                                    </a>
+                                </h4>
+                            </dt>
+                            <dd class="callout__link-text">
+                                <?php echo $callout_link['text'] ?>
+                            </dd>
+                        </div>
+                    <?php } ?>
+                </dl>
+        <?php } ?>
     </div>
 </div>
