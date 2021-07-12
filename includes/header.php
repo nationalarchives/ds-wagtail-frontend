ff3943e1fbdc9b21d03ce3a2b37cd8069f5dfdfa
AshTNA <ashleyaburnett@gmail.com>
2021-07-12 15:12:58 +0100

    Changes in response to PR
    Remove empty line from CSS, fix header alignment, and improve line-height on mobile generic intro heading

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,7 @@
 <a href="#maincontent" class="skip-to-content-link sr-only sr-only-focusable">Skip to main content</a>
+<div class="container">
 <header class="tna-header">
+ 
     <a href="/" class="tna-header__home-link">
         <img src="/images/tna-blk-logo-beta.svg" class="img-fluid tna-header__logo--beta" alt="The National Archives">
     </a>
@@ -8,3 +10,4 @@
     We are the guardians of over 1,000 years of iconic documents and UK government records.
     <p>
 </header>
+</div>
