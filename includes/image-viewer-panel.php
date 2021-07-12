2854dbb374012eb3b3411e08d7963aa3e7784c3d
Gwyn Jones <gtvj@users.noreply.github.com>
2021-07-12 13:50:05 +0100

    Add data attributes to image viewer panel

--- includes/image-viewer-panel.php
+++ includes/image-viewer-panel.php
@@ -1,6 +1,6 @@
-<div class="image-viewer-panel">
+<div class="image-viewer-panel" data-container-name="image-viewer-panel">
     <div class="container">
         <img src="../images/details/dummy-image.png" alt="">
-        <a href="#" title="#">Open in our image viewer</a>
+        <a href="#" title="#" data-link="Open in our image viewer">Open in our image viewer</a>
     </div>
 </div>
