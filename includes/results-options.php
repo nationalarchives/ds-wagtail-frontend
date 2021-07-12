a1aaa77de605d2c5afd7e72d84b742ac6fdebe39
AshTNA <ashleyaburnett@gmail.com>
2021-07-12 12:58:55 +0100

    Change results options text

--- includes/results-options.php
+++ includes/results-options.php
@@ -11,7 +11,7 @@
                 <form action="">
                     <label for="sort">Sort by:</label>
                     <select class="results-options__select" name="sort" id="sort">
-                        <option value="importance">Importance</option>
+                        <option value="importance">Publication date</option>
                         <option value="else">Something else</option>
                     </select>
                     <input class="results-options__submit" type="submit">
