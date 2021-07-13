beaca155af5229cec2b3cf946d5addf6dd1612d5
JamesWChan <jwchan1993@gmail.com>
2021-07-13 08:40:53 +0100

    Implement insights badge html

--- includes/key-facts__author.php
+++ includes/key-facts__author.php
@@ -5,7 +5,7 @@
             if(!empty($author['image'])) { ?>
                 <div class="col-md-4 col-sm-12">
                     <div class="key-facts__author-image-container">
-                        <img class="key-facts__author-image" src="<?= $author['image']?>" alt="" />
+                        <?php require '../includes/insights-badge.php' ?>
                     </div>
                 </div>
                 <div class="col-md-8 col-sm-12">
