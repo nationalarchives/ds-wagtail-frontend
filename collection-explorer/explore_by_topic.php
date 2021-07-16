df0d6d62b57e6b413a1fbf39a51180ba99bed442
AshTNA <ashleyaburnett@gmail.com>
2021-07-16 10:54:14 +0100

    Sync topic index page with ds-wagtail

--- collection-explorer/explore_by_topic.php
+++ collection-explorer/explore_by_topic.php
@@ -12,6 +12,25 @@
 
 <main id="maincontent">
     <?php require '../includes/generic_intro.php' ?>
+
+    <div class="container mt-4">
+        <div class="row">
+            <h2 class="sr-only"><?php echo $page['screen_reader_h2'] ?></h2>
+                <ul class="card-group--list-style-none">
+                    <?php render_secondary_nav_cards($collection_highlights_cards); ?>
+                </ul>
+            </div>
+        <div class="row">
+            <div class="col-md-12" id="content">
+                <h2><?php echo $page['other_filter_heading'] ?></h2>
+                <ul class="card-group--no-flex">
+                    <?php include "../includes/card-group-promo.php"; ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
     <div class="container">
         <div class="row">
             <div class="col-md-12" id="content">
