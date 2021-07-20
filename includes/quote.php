7a4e662e94a1990353cf5d1c75cf3f47d2fa5850
AshTNA <ashleyaburnett@gmail.com>
2021-07-20 15:14:32 +0100

    Remove reliance on 100vw
    This gives components their own bootstrap containers if they need padding, with any full-width components sitting outside of these containers.

--- includes/quote.php
+++ includes/quote.php
@@ -1,27 +1,29 @@
 <div class="quote">
-    <img class="quote__icon" src="<?= $quote_values['icon']?>" alt="" />
-    <h3 class="sr-only"><?php echo $quote_values['heading'] ?></h3>
-    <?php
-    if(!empty($quote_values['citation'])) { ?>
-        <figure>
+    <div class="container">
+        <img class="quote__icon" src="<?= $quote_values['icon']?>" alt="" />
+        <h3 class="sr-only"><?php echo $quote_values['heading'] ?></h3>
+        <?php
+        if(!empty($quote_values['citation'])) { ?>
+            <figure>
+                <blockquote class="quote__container">
+                    <p class="quote__text">
+                        <?php echo $quote_values['text'] ?>
+                    </p>
+                </blockquote>
+                <figcaption class="quote__citation">
+                    <cite>
+                        <?php echo $quote_values['citation'] ?>
+                    </cite>
+                </figcaption>
+            </figure>
+        <?php }
+
+        else { ?>
             <blockquote class="quote__container">
                 <p class="quote__text">
                     <?php echo $quote_values['text'] ?>
                 </p>
             </blockquote>
-            <figcaption class="quote__citation">
-                <cite>
-                    <?php echo $quote_values['citation'] ?>
-                </cite>
-            </figcaption>
-        </figure>
-    <?php }
-
-    else { ?>
-        <blockquote class="quote__container">
-            <p class="quote__text">
-                <?php echo $quote_values['text'] ?>
-            </p>
-        </blockquote>
-    <?php } ?>
+        <?php } ?>
+    </div>
 </div>
--- includes/research-resources.php
+++ includes/research-resources.php
@@ -1,35 +1,37 @@
 <div class="research-resources" data-container-name="research-resources">
-    <img class="research-resources__icon" src="<?php echo $research_resources_values['icon'] ?>" alt="" />
-    <p class="research-resources__icon-label" role="presentation" aria-hidden="true">
-        <?php echo $research_resources_values['icon-label'] ?>
-    </p>
-
-    <h3 class="research-resources__heading">
-        <?php echo $research_resources_values['heading'] ?>
-        <span class="sr-only" >
+    <div class="container">
+        <img class="research-resources__icon" src="<?php echo $research_resources_values['icon'] ?>" alt="" />
+        <p class="research-resources__icon-label" role="presentation" aria-hidden="true">
             <?php echo $research_resources_values['icon-label'] ?>
-        </span>
-    </h3>
+        </p>
 
-    <p class="research-resources__description">
-        <?php echo $research_resources_values['description'] ?>
-    </p>
+        <h3 class="research-resources__heading">
+            <?php echo $research_resources_values['heading'] ?>
+            <span class="sr-only" >
+                <?php echo $research_resources_values['icon-label'] ?>
+            </span>
+        </h3>
 
-    <dl class="research-resources__list">
-        <?php
-        foreach ($research_resources_values['links'] as $research_resources_links => $research_resources_link) { ?>
-            <div class="research-resources__list-item">
-                <dt>
-                    <h4 class="research-resources__link-heading">
-                        <a class="research-resources__link" href="<?php echo $research_resources_link['link'] ?>" data-link="<?php echo $research_resources_link['index'] . " : " . $research_resources_link['heading'] ?>">
-                            <?php echo $research_resources_link['heading'] ?>
-                        </a>
-                    </h4>
-                </dt>
-                <dd class="research-resources__link-text">
-                    <?php echo $research_resources_link['text'] ?>
-                </dd>
-            </div>
-        <?php } ?>
-    </dl>
+        <p class="research-resources__description">
+            <?php echo $research_resources_values['description'] ?>
+        </p>
+
+        <dl class="research-resources__list">
+            <?php
+            foreach ($research_resources_values['links'] as $research_resources_links => $research_resources_link) { ?>
+                <div class="research-resources__list-item">
+                    <dt>
+                        <h4 class="research-resources__link-heading">
+                            <a class="research-resources__link" href="<?php echo $research_resources_link['link'] ?>" data-link="<?php echo $research_resources_link['index'] . " : " . $research_resources_link['heading'] ?>">
+                                <?php echo $research_resources_link['heading'] ?>
+                            </a>
+                        </h4>
+                    </dt>
+                    <dd class="research-resources__link-text">
+                        <?php echo $research_resources_link['text'] ?>
+                    </dd>
+                </div>
+            <?php } ?>
+        </dl>
+    </div>
 </div>
