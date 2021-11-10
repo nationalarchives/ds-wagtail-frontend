bfbd22406f238b252ba8910fc0b07c852cfb5974
Matt Blair <mdrblair@gmail.com>
2021-11-10 14:42:17 +0000

    Amending links in local hierarchy to fix contextual links accessibility issue (#172)

--- includes/hierarchy__local.php
+++ includes/hierarchy__local.php
@@ -3,26 +3,24 @@
         <div class="col-4 col-md-2">
             <p class="hierarchy-local__nav-item">
                 <span class="hierarchy-local__nav-indicator hierarchy-local__nav-indicator--left"></span>
-                <span class="hierarchy-local__nav-label">Previous</span>
-                <a href="#" data-link="Previous">C 5/139/34</a>
+                <a href="#" data-link="Previous"><span class="hierarchy-local__nav-label">Previous record</span>
+                C 5/139/34</a>
             </p>
         </div>
         <div class="col-4 col-md-8">
             <p class="hierarchy-local__nav-item">
                 <span class="hierarchy-local__nav-indicator hierarchy-local__nav-indicator--up"></span>
-                <span class="hierarchy-local__nav-label">This record is in</span>
+                <a href="#" data-link="Series level"><span class="hierarchy-local__nav-label">This record is in</span>
                 <!-- This data-link would reflect the level of the item linked to (i.e. series etc.)  -->
-                <a href="#" data-link="Series level">
-                    C 5
-                    <span class="hierarchy-local__nav-link">Court of Chancery: Six Clerks Office: Pleadings before 1714, Bridges</span>
+                    C 5 Court of Chancery: Six Clerks Office: Pleadings before 1714, Bridges
                 </a>
             </p>
         </div>
         <div class="col-4 col-md-2">
             <p class="hierarchy-local__nav-item">
                 <span class="hierarchy-local__nav-indicator hierarchy-local__nav-indicator--right"></span>
-                <span class="hierarchy-local__nav-label">Next</span>
-                <a href="#" data-link="Next">C 5/139/36</a>
+                <a href="#" data-link="Next"><span class="hierarchy-local__nav-label">Next record</span>
+                C 5/139/36</a>
             </p>
         </div>
     </div>
